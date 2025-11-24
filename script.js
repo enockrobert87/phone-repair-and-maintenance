eab400fb3d53d2a0f33947e1672411360543a03b
enockrobert87 <enockrobert87@gmail.com>
2025-11-24 09:42:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event){
+  event.preventDefault();
+  alert("Thank you for contacting Mob!leCare! We will get back to you soon.");
+});
